fa7393aeb99a88a89923de4e621fe1e35e1e3475
Aahana0123 <83539882+Aahana0123@users.noreply.github.com>
2022-04-23 13:48:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function image1(){
+    window.location="page1.html";
+}
+
+function image2(){
+    window.location="page2.html";
+}
+
+function image3(){
+    window.location="page3.html";
+}
+
+function image4(){
+    window.location="page4.html";
+}
+
+function image5(){
+    window.location="page5.html";
+}
+
